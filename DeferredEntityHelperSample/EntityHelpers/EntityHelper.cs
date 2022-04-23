b09558a8b00d0d7f99e15f40a1a43caafac9fb10
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-04-22 20:39:24 -0400

    Added Sample Project

---
+++ DeferredEntityHelperSample/EntityHelpers/EntityHelper.cs
@@ -0,0 +1,18 @@
+﻿using DeferredEntityHelper;
+using DeferredEntityHelperSample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeferredEntityHelperSample.EntityHelpers
+{
+    public class EntityHelper : BaseEntityHelper<SampleContext>
+    {
+        public EntityHelper() : base(new SampleContext())
+        {
+
+        }
+    }
+}
