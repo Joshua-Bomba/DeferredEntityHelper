89e251ab71841be7a3ec1c36ebc391ca73c171c9
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-05-21 18:48:51 -0400

    Added BaseFutureCallBackWithParams

---
+++ DeferredEntityHelper/DataBaseFutures/Callback/BaseFutureCallBackWithParams.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeferredEntityHelper.DataBaseFutures.Callback
+{
+    public abstract class BaseFutureCallBackWithParams<T> : IFutureCallback<T> where T : class
+    {
+        private IFuture[] _wait;
+        public BaseFutureCallBackWithParams(IFuture[] wait)
+        {
+            _wait = wait;
+        }
+
+        public abstract Task<PotentialFuture<T>> Callback();
+
+        public bool DepedenciesResolved() => !_wait.Any(x => !x.Resolved);
+    }
+}
