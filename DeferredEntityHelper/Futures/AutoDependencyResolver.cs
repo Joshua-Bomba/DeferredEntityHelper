a2633b6d18294073a190ea2047224b3586200970
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-05-21 21:49:21 -0400

    AutoDependencyResolver

---
+++ DeferredEntityHelper/Futures/AutoDependencyResolver.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeferredEntityHelper.Futures
+{
+    public class AutoDependencyResolver : DependencyResolver, IAutoDependencyResolver
+    {
+        private Func<Task> _triggerResolveOperations;
+        public AutoDependencyResolver(Func<Task> triggerResolveOperation)
+        {
+            _triggerResolveOperations = triggerResolveOperation;
+        }
+
+        public async ValueTask DisposeAsync() => await this.TriggerResolves(_triggerResolveOperations);
+    }
+}
