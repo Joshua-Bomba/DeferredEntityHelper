b3e285e0f3fc60bd74fdf6496a588e1fe9759f2b
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-05-23 15:06:06 -0400

    let's lock the AddEntityAsync

--- DeferredEntityHelper/ConcurrentBaseEntityHelper.cs
+++ DeferredEntityHelper/ConcurrentBaseEntityHelper.cs
@@ -1,17 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using DeferredEntityHelper.Futures;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nito.AsyncEx;
 
 namespace DeferredEntityHelper
 {
-    public abstract class ConcurrentBaseEntityHelper<T> : BaseEntityHelper<T> where T : DbContext
+    public class ConcurrentBaseEntityHelper<T> : BaseEntityHelper<T> where T : DbContext
     {
+        private AsyncLock _lock;
         public ConcurrentBaseEntityHelper(T context) : base(context)
         {
-
+            _lock = new AsyncLock();
         }
+
+
+        public override async Task<FutureDetermined<TProp>> AddEntityAsync<TProp>(TProp e, Func<TProp, Task> actionPostSave = null)
+        {
+            using(await _lock.LockAsync())
+            {
+                return await base.AddEntityAsync(e, actionPostSave);
+            }
+        }
+
     }
 }
