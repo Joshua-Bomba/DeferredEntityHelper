42a9ca39b4d6fec6198fcffdeae0c5ee22ee275c
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-05-23 15:02:20 -0400

    added a new class to handle concurrent db stuff

---
+++ DeferredEntityHelper/ConcurrentBaseEntityHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeferredEntityHelper
+{
+    public abstract class ConcurrentBaseEntityHelper<T> : BaseEntityHelper<T> where T : DbContext
+    {
+        public ConcurrentBaseEntityHelper(T context) : base(context)
+        {
+
+        }
+    }
+}
