43abb74708ef265a65612c01c09b3ecc902df0df
Joshua Bomba <12107070+Joshua-Bomba@users.noreply.github.com>
2022-05-28 21:03:33 -0400

    add these files off the master head

--- DeferredEntityHelperSample/EntityHelpers/IModel4Helper.cs
+++ DeferredEntityHelperSample/EntityHelpers/IModel4Helper.cs
@@ -6,12 +6,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using DeferredEntityHelper.IndexedCachedModels;
+using DeferredEntityHelper.Futures.Callback;
 
 namespace DeferredEntityHelperSample.EntityHelpers
 {
     public interface IModel4Helper
     {
-        Task<PotentialFuture<Model4>> CreateModel4IfItDoesNotExist(string type, string secondValue);
+        Task<PotentialFuture<Model4>> CreateModel4IfItDoesNotExist(string type, string secondValue, IFutureCallback<Model4> test = null);
 
         Task<PotentialFuture<Model1>> CreateModel4IfItDoesNotExistAndReturnModel1Attached(string type, string secondValue, string d);
     }
@@ -23,7 +24,7 @@ namespace DeferredEntityHelperSample.EntityHelpers
 
 
 
-        async Task<PotentialFuture<Model4>> IModel4Helper.CreateModel4IfItDoesNotExist(string type, string secondValue)
+        async Task<PotentialFuture<Model4>> IModel4Helper.CreateModel4IfItDoesNotExist(string type, string secondValue,IFutureCallback<Model4> test)
         {
             //First we check if the item is in the DataBase
             IFutureDetermined<Model4>? m4 = await this._cacheManager.GetCachedIndexedDictionary<string,Model4>(x=>x.Type).AtKey(type);
@@ -40,7 +41,15 @@ namespace DeferredEntityHelperSample.EntityHelpers
                 //this will wrap it in a DatabaseFutureDetermined
                 //this means we have access to the object via the GetUnresolvedItem() method
                 //this means we could pass this into something else and it can modify it before SaveChanges is called
-                return await this.AddEntityAsync(newM);
+                if(test == null)
+                {
+                    return await this.AddEntityAsync(newM);
+                }
+                else
+                {
+                    return await this.AddEntityAsync(newM, test);
+                }
+
 
             }
             else
@@ -68,16 +77,21 @@ namespace DeferredEntityHelperSample.EntityHelpers
                     Type = type,
                     ASecondValue = secondValue
                 };
-                FutureDetermined<Model4> m4Future = await this.AddEntityAsync(model);
 
-                return await this.WaitForPromises<Model1,Model4>(async x =>{
+                DependencyBridge<Model4,Model1> bridge = new DependencyBridge<Model4,Model1>(async x => {
                     Model1 newM = new Model1
                     {
                         SomethingUnique = d,
                         Model4 = x
                     };
                     return await this.AddEntityAsync(newM);
-                },m4Future);
+                });
+                FutureDetermined<Model4> m4Future = await this.AddEntityAsync(model,bridge);
+
+                return await this.WaitForPromises<Model1,Model1>(async x =>
+                {
+                    return x;
+                }, bridge);
 
 
             }
